b775169305eccea15272c24361cc762d0dfa2518
Riuminjake <64644764+Riuminjake@users.noreply.github.com>
2022-02-25 19:59:24 +0300

    Second Branch

--- src/main/java/oop/One.java
+++ src/main/java/oop/One.java
@@ -3,5 +3,8 @@ package oop;
 public class One {
     public static void main(String[] args) {
         System.out.println("hell");
+        System.out.println("help");
+        System.out.println("hello");
+        System.out.println(String.format("fsdfsg %d",23));
     }
 }
